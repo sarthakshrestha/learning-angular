2f6eb5f5bc9bc43cf8a1e15c57e1ac16d09f0357
Sarthak Shrestha <sarthakstha24@gmail.com>
2025-04-03 13:33:33 +0545

    finished w pipe for filtering

--- src/app/pipes/filter-todos.pipe.ts
+++ src/app/pipes/filter-todos.pipe.ts
@@ -1,12 +1,18 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Todo } from '../model/todo.type';
 
 @Pipe({
-  name: 'filterTodos'
+  name: 'filterTodos',
 })
 export class FilterTodosPipe implements PipeTransform {
+  transform(todos: Todo[], searchTerm: string): Todo[] {
+    if (!searchTerm) {
+      return todos;
+    }
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+    const text = searchTerm.toLocaleLowerCase();
+    return todos.filter((todo) => {
+      return todo.title.toLowerCase().includes(text);
+    });
   }
-
 }
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -4,19 +4,22 @@ import { Todo } from '../model/todo.type';
 import { catchError } from 'rxjs';
 import { NgIf } from '@angular/common';
 import { TodoItemComponent } from '../components/todo-item/todo-item.component';
+import { FormsModule } from '@angular/forms';
+import { FilterTodosPipe } from '../pipes/filter-todos.pipe';
 
 @Component({
   selector: 'app-todos',
-  imports: [TodoItemComponent],
+  imports: [TodoItemComponent, FormsModule, FilterTodosPipe],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss',
 })
 export class TodosComponent implements OnInit {
   todoService = inject(TodosService);
   todoItems = signal<Array<Todo>>([]);
+  searchTerm = signal('');
 
   ngOnInit(): void {
-    // console.log(this.todoService.todoItems);
+  // console.log(this.todoService.todoItems);
     this.todoService
       .getTodosFromApi()
       .pipe(
